0bbf039e572eb4f9672848069e5f158adc2d497b
mghz <m1gharzed@gmail.com>
2020-03-14 18:28:11 -0400

    updated configs

--- setup.sh
+++ setup.sh
@@ -12,16 +12,29 @@ ln -sfv ~/dotfiles/shell/bash_profile $HOME/.bash_profile
 ln -sfv ~/dotfiles/shell/bashrc $HOME/.bashrc
 ln -sfv ~/dotfiles/git/gitconfig $HOME/.gitconfig
 
+# check for os
+if [[ `uname` == "Darwin" ]]
+then
+  #config_mac
+  echo "loading mac config"
+elif [[ `uname` == "Linux" ]]
+then
+  echo "loading mac config"
+  #config_linux
+fi
+
 ########### linux setup #################
 
-# update distro
+function config_linux() {
+
+# update
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove
 sudo apt autoclean
 
 # install software
-sudo apt install git curl neovim ctags -y
+sudo apt install git curl neovim ctags snapd -y
 
 # install vim-plug plugin manager
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -30,12 +43,14 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.g
 # (https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts) (I'll be using Iosevka for Powerline)
 curl -fLo ~/.fonts/Iosevka\ Term\ Nerd\ Font\ Complete.ttf --create-dirs https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Iosevka/Regular/complete/Iosevka%20Term%20Nerd%20Font%20Complete.ttf
 
+}
 
 ########### mac setup #################
 
+function config_mac() {
+
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
 # run brew bundle
 cd mac
 brew bundle
@@ -45,7 +60,9 @@ cd ..
 # install vim plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
-
 # configure fuzzy file finder
 (brew --prefix)/opt/fzf/install
+
+}
+
+
