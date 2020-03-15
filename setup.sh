91ba91f131f20501a0fa893c0bed09511a7a4f1f
mghz <m1gharzed@gmail.com>
2020-03-15 12:38:12 -0400

    updated setup

--- setup.sh
+++ setup.sh
@@ -6,63 +6,95 @@
 mkdir -p ~/.config/nvim
 
 # setup links
-ln -sfv ~/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -sfv ~/dotfiles/nvim/init.vim $HOME/.vimrc
-ln -sfv ~/dotfiles/shell/bash_profile $HOME/.bash_profile
-ln -sfv ~/dotfiles/shell/bashrc $HOME/.bashrc
-ln -sfv ~/dotfiles/git/gitconfig $HOME/.gitconfig
 
 # check for os
-if [[ `uname` == "Darwin" ]]
+if [ `uname` == "Darwin" ]
 then
   #config_mac
   echo "loading mac config"
-elif [[ `uname` == "Linux" ]]
+elif [ `uname` == "Linux" ]
 then
   echo "loading mac config"
   #config_linux
 fi
 
+# config_links
+
 ########### linux setup #################
 
-function config_linux() {
+function linux_config() {
+	
+  # update distro
+  sudo apt update
+  sudo apt upgrade -y
+  sudo apt autoremove
+  sudo apt autoclean
+  
+  # install software
+  sudo apt install zsh git curl neovim ctags snapd -y
 
-# update
-sudo apt update
-sudo apt upgrade -y
-sudo apt autoremove
-sudo apt autoclean
+  # install zprezto zsh
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
 
-# install software
-sudo apt install git curl neovim ctags snapd -y
+  # switch to new shell
+  chsh -s $(which zsh)
 
-# install vim-plug plugin manager
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  # install vim-plug plugin manager
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  
+  # (optional but recommended) install a nerd font for icons and a beautiful airline bar
+  # (https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts) (I'll be using Iosevka for Powerline)
+  curl -fLo ~/.fonts/Iosevka\ Term\ Nerd\ Font\ Complete.ttf --create-dirs \ 
+	  https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Iosevka/Regular/complete/Iosevka%20Term%20Nerd%20Font%20Complete.ttf
 
-# (optional but recommended) install a nerd font for icons and a beautiful airline bar
-# (https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts) (I'll be using Iosevka for Powerline)
-curl -fLo ~/.fonts/Iosevka\ Term\ Nerd\ Font\ Complete.ttf --create-dirs https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/Iosevka/Regular/complete/Iosevka%20Term%20Nerd%20Font%20Complete.ttf
+}
+
+function linux_zsh() {
+
 
 }
 
 ########### mac setup #################
 
-function config_mac() {
+function mac_config() {
 
-# install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-# run brew bundle
-cd mac
-brew bundle
+  # install brew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-cd ..
-
-# install vim plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# configure fuzzy file finder
-(brew --prefix)/opt/fzf/install
-
+  # run brew bundle
+  cd mac
+  brew bundle
+  
+  # install vim plug
+  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  
+  # configure fuzzy file finder
+  (brew --prefix)/opt/fzf/install
+  
 }
+  
+########### mac setup #################
 
-
+function config_links() {
+    
+  # vim/nvim
+  ln -sfv ~/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+  ln -sfv ~/dotfiles/nvim/init.vim $HOME/.vimrc
+  
+  # zsh
+  # ln -sfv ~/dotfiles/shell/zshell/zshrc $HOME/.zprezto/runcoms/.zshrc
+  
+  # bash
+  ln -sfv ~/dotfiles/shell/bash_profile $HOME/.bash_profile
+  ln -sfv ~/dotfiles/shell/bashrc $HOME/.bashrc
+  
+  # git
+  ln -sfv ~/dotfiles/git/gitconfig $HOME/.gitconfig
+}
